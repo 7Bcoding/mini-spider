e0a7a724f0427ed1b9d2748621d7824deb0e3710
cenquanyu <cenquanyu@baidu.com>
2020-11-10 14:58:43 +0800

    更新main方法

--- mini_spider.py
+++ mini_spider.py
@@ -1,89 +1,33 @@
 # /usr/bin/python
+################################################################################
+#
+# Copyright (c) 2020 Baidu.com, Inc. All Rights Reserved
+#
+################################################################################
 
-import configparser
-import os
-from queue import Queue
-from lxml import etree
-from bs4 import BeautifulSoup
-import argparse
-import requests
 import log
+from worker import param_parse
+from worker import spider_engine
 
 
-class mini_spider(object):
-    def __init__(self, *args, **kwargs):
-        params = args[0]
-        self.urls = params[0]
-        self.headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36'
-                                      ' (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36'}
-        self.result = params[1]
-        self.maxdepth = params[2]
-        self.interval = params[3]
-        self.timeout = params[4]
-        self.thread_count = params[5]
-        self.target_url = ".*\.(gif|png|jpg|bmp)$"
-        self.urlqueue = Queue()
-
-    def getpages(self):
-        url_arr = []  # 首页链接列表
-        try:
-            response = requests.get(self.urls, self.headers['User-Agent'])
-            soup = BeautifulSoup(response.text, 'html.parser')
-            url_list = soup.select('a')
-            for links in url_list:
-                if links.has_attr('href'):
-                    self.urlqueue.put(self.urls + links['href'])
-                    url_arr.append(self.urls + links['href'])
-            urlarr = list(set(url_arr))
-            print(len(urlarr), urlarr)
-            self.page_detail()
-            # log.info('there is the page:')
-            # log.info(urlarr)
-        except Exception as e:
-            log.error(e)
-
-    def page_detail(self):
-        urls = self.urlqueue.get()
-        print(urls, '详细内容为：')
-        response = requests.get(urls, self.headers['User-Agent'])
-        soup = BeautifulSoup(response.text, 'html.parser')
-        url_list = soup.select('a')
-        for links in url_list:
-            if links.has_attr('href'):
-                print(links['href'], end=',')
-
-
-class configparse(object):
-    def parse(self, config_file):
-        config = configparser.ConfigParser()
-        config.read(config_file, encoding='utf-8')
-        urls = config['spider']['feedfile']  # feedfile path
-        result = config['spider']['result']  # result storage file
-        max_depth = config['spider']['max_depth']  # max scratch depth
-        crawl_interval = config['spider']['crawl_interval']  # scratch interval
-        crawl_timeout = config['spider']['crawl_timeout']  # scratch timeout
-        thread_count = config['spider']['thread_count']  # scratch thread
-        return urls, result, max_depth, crawl_interval, crawl_timeout, thread_count
-
-
-class argsparse(object):
-    def getargs(self):
-        parser = argparse.ArgumentParser(prog='mini_spider',
-                                         usage='minispider using method',
-                                         description='mini_spider is a Multithreaded crawler')
-        parser.add_argument('-c', '--conf', help='config_file')
-        parser.add_argument('-v', '--version', help='version', action="store_true")
-        args = parser.parse_args()
-        if args.version:
-            print('mini-spider_v1.0')
-        else:
-            return args
+def main():
+    """
+    the main method to run mini spider
+    """
+    # 配置文件参数初始化
+    conf_parser = param_parse.config_parser()
+    argsparser = param_parse.args_parser()
+    # 获取控制台输入参数
+    args = argsparser.get_args()
+    # 初始化日志配置
+    log.init_log('./mini_spider')
+    if args:
+        # 读取配置文件spider.conf，获取参数
+        conf_params = conf_parser.set_config_by_file(args.conf)
+        # 通过配置文件设置爬虫初始化参数
+        spider = spider_engine.SpiderEngine(conf_params)
+        spider.getpages()
 
 
 if __name__ == '__main__':
-    args = argsparse().getargs()
-    log.init_log('./mini_spider')
-    if args:
-        conf_params = configparse().parse(args.conf)
-        spider = mini_spider(conf_params)
-        spider.getpages()
+    main()
